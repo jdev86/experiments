56a69b532dd1e60fb6a815d988fd057dc662a857
jdev86 <justin.m.mills@outlook.com>
2023-01-11 19:57:14 -0600

    Adding next-frontend app

--- apps/next-frontend/pages/_app.tsx
+++ apps/next-frontend/pages/_app.tsx
@@ -1,15 +1,20 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import './styles.css';
+import Layout from './layout';
 
 function CustomApp({ Component, pageProps }: AppProps) {
+  // const border = store.hours < 12 ? "linear-gradient(to right, #032642, #05518e, #0d73c6)" : "linear-gradient(to right, #ed692d, #f2b04d, #efc36b)"
+
   return (
     <>
       <Head>
         <title>Welcome to next-frontend!</title>
       </Head>
       <main className="app">
-        <Component {...pageProps} />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
       </main>
     </>
   );
